a49acc9ffe452ab3cdacac29cae20b243045a0a2
charllab <developer.charl@gmail.com>
2023-02-10 10:11:31 -0800

    added council custom post type integration

---
+++ src/themes/pib/page-council.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+    <main>
+        <?php get_template_part('partials/head/flexible-content'); ?>
+        <?php get_template_part('partials/cards/team'); ?>
+        <?php get_template_part('partials/cards/quick-links'); ?>
+        <?php get_template_part('partials/foot/flexible-content'); ?>
+    </main>
+
+<?php get_footer(); ?>
--- src/themes/pib/partials/cards/team.php
+++ src/themes/pib/partials/cards/team.php
@@ -1,67 +1,67 @@
-<?php ?>
+<?php
+
+$posts = get_posts(array(
+    'post_type' => 'council',
+    'order' => 'ASC',
+    'posts_per_page' => -1
+));
+
+
+?>
 
 <section class="section-group">
 
-            <section class="section-md">
-                <div class="container">
+    <section class="section-md">
+        <div class="container">
 
-                    <div class="row">
-                        <div class="col text-center">
-                            <h2 class="mb-2">Optional section title</h2>
+            <div class="row">
+                <div class="col text-center">
+                    <h2 class="mb-2"><?php the_field('secondary_title');?></h2>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+
+
+                <?php
+                foreach ($posts as $i => $post) :
+                    setup_postdata($post);
+                    //echo '<pre>';
+                    //var_dump($post);
+                    //echo '</pre>';
+                    ?>
+                    <div class="col-md-6 col-lg-4 col-xl-3 col-card">
+                        <div class="card text-center mb-2 mb-xl-0">
+                            <?php if(get_field('photo')):
+                                $image = get_field('photo');
+                                ?>
+                                <img src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>"
+                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
+                            <?php else: ?>
+                                <img src="<?php bloginfo('template_url'); ?>/images/profile-432by432.svg" alt=" "
+                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
+                            <?php endif; ?>
+                            <div class="card-body pb-2">
+                                <h3 class="h4 card-title"><?php the_title();?></h3>
+                                <p class="card-text medium mb-0"><?php the_field('role');?></p>
+                                <?php if(get_field('alternate')):?>
+                                <p class="card-text medium mb-0 font-weight-bold">Alternate:</p>
+                                <p class="card-text medium mb-0"><?php the_field('alternate');?></p>
+                                <?php endif; ?>
+                            </div>
                         </div>
-                    </div>
+                    </div><!-- col col-card -->
 
-                    <div class="row justify-content-center">
-                        <div class="col-md-6 col-lg-4 col-xl-3 col-card">
-                            <div class="card text-center mb-2 mb-xl-0">
-                                <img src="https://via.placeholder.com/456x456" alt=""
-                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
-                                <div class="card-body pb-2">
-                                    <h3 class="h4 card-title">Chief Greg Gabriel</h3>
-                                    <p class="card-text medium mb-0">Internal Government Affairs, Economic Development
-and All Portfolios</p>
-                                </div>
-                            </div>
-                        </div><!-- col col-card -->
-                        <div class="col-md-6 col-lg-4 col-xl-3 col-card">
-                            <div class="card text-center mb-2 mb-xl-0">
-                                <img src="https://via.placeholder.com/456x456" alt=""
-                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
-                                <div class="card-body pb-2">
-                                    <h3 class="h4 card-title">Dolly Kruger</h3>
-                                    <p class="card-text medium mb-0">Infrastructure, Housing, Public Works</p>
-                                    <p class="card-text medium mb-0 font-weight-bold">Alternate: </p>
-                                    <p class="card-text medium mb-0">Education, Culture & Language</p>
-                                </div>
-                            </div>
-                        </div><!-- col col-card -->
-                        <div class="col-md-6 col-lg-4 col-xl-3 col-card">
-                            <div class="card text-center mb-2 mb-xl-0">
-                                <img src="https://via.placeholder.com/456x456" alt=""
-                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
-                                <div class="card-body">
-                                    <h3 class="h4 card-title">Card title</h3>
-                                    <p class="card-text medium">This is a wider card with supporting text below as a
-                                        natural lead-in to additional content. This card has even longer content than
-                                        the first to show that equal height action.</p>
-                                </div>
-                            </div>
-                        </div><!-- col col-card -->
-                        <div class="col-md-6 col-lg-4 col-xl-3 col-card">
-                            <div class="card text-center mb-2 mb-xl-0">
-                                <img src="https://via.placeholder.com/456x456" alt=""
-                                     class="card-img-top rounded-circle w-75 mx-auto mt-2">
-                                <div class="card-body">
-                                    <h3 class="h4 card-title">Page Title</h3>
-                                    <p class="card-text medium">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                        elit.</p>
-                                </div>
-                            </div>
-                        </div><!-- col col-card -->
 
-                    </div><!-- row -->
-                </div><!-- container -->
+                    <?php
+                    wp_reset_postdata();
+                endforeach;
+                ?>
 
-            </section>
 
-        </section><!-- section-group -->
+            </div><!-- row -->
+        </div><!-- container -->
+
+    </section>
+
+</section><!-- section-group -->
